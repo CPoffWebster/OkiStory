8b9a2f05b3ed1b68e052fe91cf40b6231c3be16a
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-09-03 15:04:46 -0400

    Flipping pages implemented, still needs some small CSS changes

--- reading-alpha/src/components/BookReader.tsx
+++ reading-alpha/src/components/BookReader.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Book } from "../classes/book";
+import { FlippingPages } from "flipping-pages";
+import "flipping-pages/dist/style.css";
+import "./BookReader.css"; // Make sure to include your CSS
 
 interface ReadBook {
   fullBook: Book;
@@ -10,40 +13,56 @@ const BookReader: React.FC<ReadBook> = ({ fullBook }) => {
   const book = fullBook!.generatedBook!;
   const totalPages = book.pages.length;
 
+  // For flipping pages
+  const [selected, setSelected] = useState(0);
+
+  // Update currentPage when selected changes and vice versa
+  useEffect(() => {
+    setCurrentPage(selected);
+  }, [selected]);
+
+  useEffect(() => {
+    setSelected(currentPage);
+  }, [currentPage]);
+
+  const back = () => {
+    setCurrentPage(Math.max(currentPage - 1, 0));
+  };
+
+  const next = () => {
+    setCurrentPage(Math.min(currentPage + 1, totalPages - 1));
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <div className="left">
-          <img
-            src={
-              currentPage === 0
-                ? book.cover
-                : book.pages[currentPage - 1].picture
-            }
-            alt="cover"
-          />
-        </div>
-        <div className="right">
-          {currentPage === 0 ? (
+        <FlippingPages
+          direction="right-to-left"
+          onSwipeEnd={setSelected}
+          selected={selected}
+        >
+          <div className="page">
+            <img src={book.cover} alt="cover" />
             <h1>{book.title}</h1>
-          ) : (
-            <p>{book.pages[currentPage - 1].text}</p>
-          )}
-        </div>
+          </div>
+          {book.pages.map((page, index) => (
+            <div className="page" key={index}>
+              <img src={page.picture} alt={`page-${index}`} />
+              <p>{page.text}</p>
+            </div>
+          ))}
+        </FlippingPages>
       </div>
       <div className="navigation">
-        <button
-          disabled={currentPage <= 0}
-          onClick={() => setCurrentPage(currentPage - 1)}
-        >
+        <button disabled={currentPage <= 0} onClick={back}>
           ←
         </button>
-        <button
-          disabled={currentPage >= totalPages}
-          onClick={() => setCurrentPage(currentPage + 1)}
-        >
+        <button disabled={currentPage >= totalPages - 1} onClick={next}>
           →
         </button>
+      </div>
+      <div className="page-count">
+        Page {currentPage + 1} of {totalPages}
       </div>
     </div>
   );
