ccfd1301af57e2545062f8fa78308e13fc4328f0
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-09-06 21:19:04 -0400

    Icons configured, UserSignIn page done for now

--- app/components/Login/UserSignIn.tsx
+++ app/components/Login/UserSignIn.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { appleIcon, facebookIcon, googleIcon } from "@/data/icons";
 import "./UserSignIn.css";
 
 const UserSignIn = () => {
   return (
     <div className="container">
-      <div className="logo">Logo Here</div> {/* Logo placement */}
+      <div className="logo">Logo Here</div> {/* todo Logo placement */}
       <h1 className="title">Reading Alpha</h1>
       <div className="email">
         <label htmlFor="email" className="label">
@@ -16,19 +17,29 @@ const UserSignIn = () => {
       <div className="signup">
         Don&apos;t have an account? <a href="/signup">Sign up</a>
       </div>{" "}
-      {/* Sign-up link */}
+      {/* todo Sign-up link */}
       <div className="orSeparator">
         <span className="line"></span>
         <span className="orText">OR</span>
         <span className="line"></span>
       </div>
-      <button className="socialButton facebook">Continue with Facebook</button>
-      <button className="socialButton google">Continue with Google</button>
-      <button className="socialButton apple">Continue with Apple</button>
+      <button className="socialButton google">
+        {googleIcon}
+        Continue with Google
+      </button>
+      <button className="socialButton facebook">
+        {facebookIcon}
+        Continue with Facebook
+      </button>
+      <button className="socialButton apple">
+        {appleIcon}
+        Continue with Apple
+      </button>
       <div className="footer">
         <a href="/terms">Terms of use</a> |{" "}
         <a href="/privacy">Privacy policy</a>
       </div>
+      {/* todo terms / privacy */}
     </div>
   );
 };
