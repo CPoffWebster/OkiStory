d331a88ba240249bf4272029d1c012c0bd166201
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-09-06 22:21:49 -0400

    Progress made (animation finally working!!!) - need to work on resizing issues

--- app/components/Login/UserSignIn.tsx
+++ app/components/Login/UserSignIn.tsx
@@ -3,38 +3,54 @@ import { appleIcon, facebookIcon, googleIcon } from "@/data/icons";
 import "./UserSignIn.css";
 
 const UserSignIn = () => {
+  const [emailValue, setEmailValue] = React.useState("");
+
+  const handleChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setEmailValue(e.target.value);
+  };
+
   return (
-    <div className="container">
-      <div className="logo">Logo Here</div> {/* todo Logo placement */}
-      <h1 className="title">Reading Alpha</h1>
-      <div className="email">
-        <label htmlFor="email" className="label">
-          Email Address
-        </label>
-        <input id="email" type="email" className="input" />
+    <div className="main-wrap">
+      <div className="container">
+        <div className="logo">Logo Here</div> {/* todo Logo placement */}
+        <h1 className="title">Reading Alpha</h1>
+        <div className="input-container">
+          <input
+            className="email-input"
+            id="email"
+            type="email"
+            value={emailValue}
+            onChange={handleChange}
+          />
+          <label className={emailValue ? "filled" : ""} htmlFor="email">
+            Email Address
+          </label>
+        </div>
+        <button className="continueButton">Continue</button>
+        <div className="signup">
+          Don&apos;t have an account? <a href="/signup">Sign up</a>
+        </div>{" "}
+        {/* todo Sign-up link */}
+        <div className="orSeparator">
+          <span className="line"></span>
+          <span className="orText">OR</span>
+          <span className="line"></span>
+        </div>
+        <button className="socialButton google">
+          {googleIcon}
+          Continue with Google
+        </button>
+        <button className="socialButton facebook">
+          {facebookIcon}
+          Continue with Facebook
+        </button>
+        <button className="socialButton apple">
+          {appleIcon}
+          Continue with Apple
+        </button>
       </div>
-      <button className="continueButton">Continue</button>
-      <div className="signup">
-        Don&apos;t have an account? <a href="/signup">Sign up</a>
-      </div>{" "}
-      {/* todo Sign-up link */}
-      <div className="orSeparator">
-        <span className="line"></span>
-        <span className="orText">OR</span>
-        <span className="line"></span>
-      </div>
-      <button className="socialButton google">
-        {googleIcon}
-        Continue with Google
-      </button>
-      <button className="socialButton facebook">
-        {facebookIcon}
-        Continue with Facebook
-      </button>
-      <button className="socialButton apple">
-        {appleIcon}
-        Continue with Apple
-      </button>
       <div className="footer">
         <a href="/terms">Terms of use</a> |{" "}
         <a href="/privacy">Privacy policy</a>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import HomePage from "./components/HomePage";
 import UserSignIn from "./components/Login/UserSignIn";
 import "bootstrap/dist/css/bootstrap.css";
