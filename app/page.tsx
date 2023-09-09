a6aae4999bce7cd2cc4d26f54be61ebfdcf012f7
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-09-09 13:44:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import HomePage from "./components/HomePage";
-import UserSignIn from "./components/Login/UserSignIn";
+// import UserSignIn from "./components/Login/UserSignIn";
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Home() {
   return (
     <>
-      {/* <HomePage /> */}
-      <UserSignIn />
+      <HomePage />
+      {/* <UserSignIn /> */}
     </>
   );
 }
