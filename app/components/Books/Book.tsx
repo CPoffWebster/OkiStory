140aa5869a9016dd9b572fe744ad38e500956e52
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-09-28 23:42:00 -0400

    Got the basic book working in react

---
+++ app/components/Books/Book.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import styles from "./Book.module.css";
+
+const Book: React.FC = () => {
+  useEffect(() => {
+    const pages = Array.from(
+      document.getElementsByClassName(styles.page)
+    ) as HTMLElement[];
+
+    pages.forEach((page, i) => {
+      if (i % 2 === 0) {
+        page.style.zIndex = `${pages.length - i}`;
+      }
+    });
+
+    pages.forEach((page, i) => {
+      page.onclick = function () {
+        if ((i + 1) % 2 === 0) {
+          page.classList.remove(styles.flipped);
+
+          // Check if previousElementSibling exists
+          if (page.previousElementSibling) {
+            (page.previousElementSibling as HTMLElement).classList.remove(
+              styles.flipped
+            );
+          }
+        } else {
+          page.classList.add(styles.flipped);
+
+          // Check if nextElementSibling exists
+          if (page.nextElementSibling) {
+            (page.nextElementSibling as HTMLElement).classList.add(
+              styles.flipped
+            );
+          }
+        }
+      };
+    });
+  }, []);
+
+  return (
+    <div className={styles.book}>
+      <div id="pages" className={styles.pages}>
+        <div className={styles.page}>
+          <p>
+            Open Me, <br />
+            please!
+          </p>
+        </div>
+        <div className={styles.page}></div>
+        <div className={styles.page}>
+          <p>Hello there!</p>
+        </div>
+        <div className={styles.page}></div>
+        <div className={styles.page}></div>
+        <div className={styles.page}></div>
+        <div className={styles.page}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
--- pages/read/[id].tsx
+++ pages/read/[id].tsx
@@ -1,60 +1,65 @@
-import { useEffect, useState } from "react";
-import styles from "./cover.module.css";
+import { getBook } from "@/services/books";
 import { BooksAttributes } from "@/services/database/models/Books";
+import { GetServerSideProps } from "next";
+import styles from "./cover.module.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Book from "@/app/components/Books/Book";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.query.id as unknown as string; // Access 'id' directly
+  const book = await getBook(id);
+  return {
+    props: { book },
+  };
+};
 
 export default function CoverPage(props: { book: BooksAttributes }) {
-  const [flippedPages, setFlippedPages] = useState<number[]>([]);
-  const totalPages = 8; // 7 inner pages + 1 cover
-
-  useEffect(() => {
-    const pages = Array.from(document.getElementsByClassName("page"));
-    for (let i = 0; i < pages.length; i++) {
-      const page = pages[i] as HTMLElement;
-      if (i % 2 === 0) {
-        page.style.zIndex = `${totalPages - i}`;
-      }
-    }
-  }, []);
-
-  const handlePageClick = (pageNum: number) => {
-    if (pageNum === 1) return; // Do not flip the cover page
-    setFlippedPages((prev) => {
-      if (prev.includes(pageNum)) {
-        return prev.filter((p) => p !== pageNum);
-      }
-      return [...prev, pageNum];
-    });
-  };
-
   return (
-    <div className={styles.container}>
-      <div className={styles.book}>
-        <div className={styles.pages}>
-          {Array.from({ length: totalPages }, (_, index) => {
-            const isFlipped = flippedPages.includes(index + 1);
-            const isCover = index === 0;
-            return (
-              <div
-                key={index}
-                className={`${styles.page} ${isFlipped ? styles.flipped : ""} ${
-                  isCover ? styles.cover : ""
-                }`}
-                onClick={() => handlePageClick(index + 1)}
-              >
-                {isCover && (
-                  <p className={styles.pageContent}>
-                    Book Cover <br />
-                    {/* {props.book.Title} */}
-                  </p>
-                )}
-                {!isCover && index === 2 && (
-                  <p className={styles.pageContent}>Hello there!</p>
-                )}
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div>
+      <Book />
     </div>
   );
+  // const [flippedPages, setFlippedPages] = useState<number[]>([]);
+  // const totalPages = 7; // Number of pages
+
+  // useEffect(() => {
+  //   const pages = Array.from(document.getElementsByClassName('page'));
+  //   for (let i = 0; i < pages.length; i++) {
+  //     const page = pages[i] as HTMLElement;
+  //     if (i % 2 === 0) {
+  //       page.style.zIndex = `${totalPages - i}`;
+  //     }
+  //   }
+  // }, []);
+
+  // const handlePageClick = (pageNum: number) => {
+  //   if (pageNum % 2 === 0) {
+  //     setFlippedPages((prev) => prev.filter((p) => p !== pageNum && p !== pageNum - 1));
+  //   } else {
+  //     setFlippedPages((prev) => [...prev, pageNum, pageNum + 1]);
+  //   }
+  // };
+
+  // return (
+  //   <div className={styles.container}>
+  //     <div className={styles.book}>
+  //       <div className={styles.pages}>
+  //         {Array.from({ length: totalPages }, (_, index) => {
+  //           const isFlipped = flippedPages.includes(index + 1);
+  //           return (
+  //             <div
+  //               key={index}
+  //               className={`${styles.page} ${isFlipped ? 'flipped' : ''}`}
+  //               onClick={() => handlePageClick(index + 1)}
+  //             >
+  //               {index === 0 && <p className={styles.pageContent}>Open Me, <br />please!</p>}
+  //               {index === 2 && <p className={styles.pageContent}>Hello there!</p>}
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
