806d70e93fc8b6d5a35476637988b521411d8552
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-10-17 17:01:11 -0400

    Added auth to story generation

--- pages/api/generation/story.ts
+++ pages/api/generation/story.ts
@@ -1,22 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import { generateText, testGenerateText } from '@/services/generation/chatGPT';
-// import { generateImage, testGenerateImage } from '@/services/generation/dalle';
 import { initializeBookCreation } from '@/services/generation/bookCreation';
+import { withAuth } from "@/utils/withAuth";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log('api/generation/story API Route Triggered');
 
     const { locationID, characterID, themeID, userID } = req.body;
-
-    // const generatedText = await generateText();
-    // const generatedText = await testGenerateText();
-    // const generatedText = await generateImage();
-    // const generatedText = await testGenerateImage();
-
-    // res.status(200).json({ generatedText });
-
-
     const guid = initializeBookCreation(locationID, characterID, themeID, userID);
 
     res.status(200).json({ bookGuid: guid });
-}
+};
+
+export default withAuth(handler);
