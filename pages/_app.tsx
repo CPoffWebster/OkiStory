d9c6dba83c67ea7528be46eaac893a25caf74e71
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2023-11-01 19:12:49 -0400

    Testing adding GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 
@@ -12,6 +13,22 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DW4563ZMBT"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DW4563ZMBT');
+            `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
--- todo.md
+++ todo.md
@@ -1,12 +1,15 @@
+Bugs:
+Viewing multiple books - error + not showing if <=3
+
 Features:
 Analytics
 Login/Logout settings
 Loading / generation screen - show pictures of hero/setting to the left
 
 Other:
+okistory.com
 stripe integration
 Example / default book - what you're allowed to do logged out - auth see hero.tsx
-okistory.com
 https://www.reddit.com/r/nextjs/comments/v9nd8k/how_to_stop_users_from_visiting_certain_pages/
 
 Styling:
