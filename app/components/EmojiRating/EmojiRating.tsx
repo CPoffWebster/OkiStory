f0ff5e4d11bd5fbda1740f83317ff5dc4653b899
Charles Poff-Webster <38145609+CPoffWebster@users.noreply.github.com>
2024-01-13 14:20:22 -0500

    Fixed previousRating and added login requirement

--- app/components/EmojiRating/EmojiRating.tsx
+++ app/components/EmojiRating/EmojiRating.tsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./EmojiRating.module.css";
 import axios from "axios";
+import { signIn, useSession } from "next-auth/react";
+import Modal from "../LoginButton/BuyCreditsModal";
+import LoginButton from "../LoginButton/LoginButton";
+import Button from "../Button/Button";
 
 const emojis = ["😠", "😦", "😐", "🙂", "😀"];
 
@@ -36,35 +40,75 @@ const EmojiRating: React.FC<EmojiRatingProps> = ({
     previousRating || 0
   );
 
+  useEffect(() => {
+    setSelectedRating(previousRating);
+  }, [previousRating]);
+
+  const session = useSession(); // session here is an object with 'data' and 'status' properties
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = () => {
+    setShowModal(!showModal); // Toggle modal visibility
+  };
+
   const updateRatingSelection = async (rating: number) => {
+    if (!!session.data === false) {
+      setShowModal(!showModal);
+      return;
+    }
     setSelectedRating(rating);
     await axios.post("/api/read/rateBook", {
       bookGuid,
       rating,
     });
-    console.log("Rating updated");
   };
 
   return (
-    <ul className={styles.emojiRating}>
-      {emojis.map((emoji, index) => {
-        const rating = index + 1;
-        const color = getColorForEmoji(index);
-        return (
-          <li
-            key={rating}
-            onClick={() => updateRatingSelection(rating)}
+    <>
+      <ul className={styles.emojiRating}>
+        {emojis.map((emoji, index) => {
+          const rating = index + 1;
+          const color = getColorForEmoji(index);
+          return (
+            <li
+              key={rating}
+              onClick={() => updateRatingSelection(rating)}
+              style={{
+                opacity: selectedRating === rating ? 1 : 0.5,
+                color: color,
+                borderColor: color,
+              }}
+            >
+              {emoji}
+            </li>
+          );
+        })}
+      </ul>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <div
             style={{
-              opacity: selectedRating === rating ? 1 : 0.5,
-              color: color,
-              borderColor: color,
+              fontFamily: "Arial, sans-serif",
+              fontSize: 30,
+              color: "#333",
+              textAlign: "center",
             }}
           >
-            {emoji}
-          </li>
-        );
-      })}
-    </ul>
+            <p>You must be logged in to rate stories!</p>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                text="Login"
+                size="small"
+                className="containerBoxSmall"
+                markedAsImportant={true}
+                onClick={() => {
+                  signIn("google").catch(console.error);
+                }}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 };
 
